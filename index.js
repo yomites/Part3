aa20c41d0cd457925c07743ff46d752274c84873
yomites <marshall_400@yahoo.com>
2020-05-30 16:48:17 +0300

    Exercise 3.9
    Exercise 3.9 Phonebook backend step 9. making the frontend work with the backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
