49576c5b2bf657ca0b4773c54c6357b86f0ae4e1
yomites <marshall_400@yahoo.com>
2020-06-04 21:11:52 +0300

    Update person.js
    Exercise 3.19, getting rid of the warning caused by the unique-validator

--- models/person.js
+++ models/person.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(result => {
         console.log('connected to MongoDB')
     }).catch((error) => {
